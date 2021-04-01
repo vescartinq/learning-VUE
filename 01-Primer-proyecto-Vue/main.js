f5e14c4f5f2b26a9b3d9125bb7655bcbb7fa13f5
Starkin28 <victoreq00@gmail.com>
2021-04-01 14:51:22 +0200

    fundamentos Vue

---
+++ 01-Primer-proyecto-Vue/main.js
@@ -0,0 +1,37 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      nombre: 'Victor',
+      edad: 39,
+      google: 'https://www.google.es',
+      estado: true,
+      cantidad: 50,
+      servicios: ['Transferencias', 'Pagos', 'Giros'],
+      desactivar: false,
+    };
+  },
+  methods: {
+    agregarSaldo() {
+      this.cantidad = this.cantidad + 100;
+    },
+    restarSaldo() {
+      this.cantidad = this.cantidad - 50;
+    },
+    disminuirSaldo() {
+      if (this.cantidad === 0 || this.cantidad < 0) {
+        alert('llegaste al final');
+        this.desactivar = true;
+        return;
+      }
+      this.cantidad = this.cantidad - 100;
+    },
+  },
+  computed: {
+    colorCantidad() {
+      return this.cantidad > 500 ? 'text-success' : 'text-danger';
+    },
+    mayusculasTexto() {
+      return this.titulo.toUpperCase();
+    },
+  },
+});
