68b76d734930290b69589e7a4859f604efea5cfb
Starkin28 <victoreq00@gmail.com>
2021-04-03 13:50:19 +0200

    incluidos estilos

--- 14-CRUD-auth-db/src/views/Ingreso.vue
+++ 14-CRUD-auth-db/src/views/Ingreso.vue
@@ -2,9 +2,19 @@
   <div>
     <h1>Ingreso de usuarios</h1>
     <form @submit.prevent="ingresoUsuario({ email: email, password: pass })">
-      <input type="email" placeholder="Ingrese email" v-model="email" />
-      <input type="password" placeholder="Ingrese contraseña" v-model="pass" />
-      <button type="submit">Acceder</button>
+      <input
+        type="email"
+        placeholder="Ingrese email"
+        v-model="email"
+        class="form-control mb-2"
+      />
+      <input
+        type="password"
+        placeholder="Ingrese contraseña"
+        v-model="pass"
+        class="form-control mb-2"
+      />
+      <button type="submit" class="btn btn-primary">Acceder</button>
     </form>
     <p>{{ error }}</p>
   </div>
---
+++ 14-CRUD-auth-db/src/views/Inicio.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Lista de Tareas</h1>
+    <router-link to="/agregar">
+      <button class="btn btn-primary mb-2">Agregar</button>
+    </router-link>
+    <ul class="list-group">
+      <li v-for="(item, index) in tareas" :key="index" class="list-group-item">
+        {{ item.id }} - {{ item.nombre }}
+        <router-link
+          :to="{ name: 'Editar', params: { id: item.id } }"
+          class="float-right ml-2"
+        >
+          <button class="btn btn-warning">Editar</button>
+        </router-link>
+        <button
+          @click="eliminarTarea(item.id)"
+          class="btn btn-danger float-right"
+        >
+          Eliminar
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex';
+export default {
+  name: 'Inicio',
+  created() {
+    this.getTareas();
+  },
+  methods: {
+    ...mapActions(['getTareas', 'eliminarTarea']),
+  },
+  computed: {
+    ...mapState(['tareas']),
+  },
+};
+</script>
